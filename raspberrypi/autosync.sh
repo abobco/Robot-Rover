81de864d8d67c289c8e1ce200b42cf9c3a66305b
Austin <abobco@gmail.com>
2021-07-19 21:52:59 -0500

    reset arm joint positions after each grab

--- raspberrypi/autosync.sh
+++ raspberrypi/autosync.sh
@@ -1,7 +1,7 @@
 echo $1
 
 while true; do 
-  inotifywait -r -e modify,create,delete /home/abobco/RaspberryPi/OCV_Projects
+  inotifywait -r -e modify,create,delete /home/abobco/RaspberryPi/Robot-Rover/raspberrypi
   # rsync -avze "ssh -i /home/abobco/.ssh/id_rsa" /home/abobco/RaspberryPi/OCV_Projects pi@$1:/home/pi/shared/novfp/RaspberryPi
-  rsync -avze "ssh -i /home/abobco/.ssh/id_rsa" /home/abobco/RaspberryPi/OCV_Projects pi@$1:/home/pi/
+  rsync -avze "ssh -i /home/abobco/.ssh/id_rsa" /home/abobco/RaspberryPi/Robot-Rover/raspberrypi pi@$1:/home/pi/
 done
